45169267965fcb7287014ee86016c8768ca6484e
manuelgh62 <adofoemmanuel62@gmail.com>
2023-06-21 19:57:35 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-	char num, letter;
+	char letter;
+	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
-		for letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
