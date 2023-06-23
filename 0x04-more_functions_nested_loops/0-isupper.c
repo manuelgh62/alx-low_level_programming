7e47ac0fefd40dba99132a5ace9aac674bfae72c
manuelgh62 <adofoemmanuel62@gmail.com>
2023-06-23 14:23:00 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,13 @@
 
 /**
  * _isupper - checks for lowercase character
- * @c: Character to be checked 
+ * @c: Character to be checked
  * Return: 1 for uppercase character or 0 for anything
  */
 int _isupper(int c)
 {
-	if( c >= 65 && c <= 90)
-	{
-		return (1);
-	}
+	if(c >= 65 && c <= 90)
+	return (1);
+	else
 	return (0);
 }
-
