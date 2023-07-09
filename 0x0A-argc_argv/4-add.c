f6d8e06e4e5b31306d289f54f2c01940c62419ee
manuelgh62 <adofoemmanuel62@gmail.com>
2023-07-09 07:43:26 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,15 @@
-
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
 	{
 		/*Declaring variables*/
 		unsigned int count;
@@ -41,7 +41,7 @@
 	 */
 
 
-	int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 
 
 	{
